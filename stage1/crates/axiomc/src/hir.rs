9753bc78552fc1c19869885b05161f5bb023e3cc
John McChesney TenEyck Jr <john.m.teneyck@gmail.com>
2026-05-23 22:55:23 +0100

    Support const fn array lengths

--- stage1/crates/axiomc/src/hir.rs
+++ stage1/crates/axiomc/src/hir.rs
@@ -588,6 +588,12 @@ fn lower_with_capabilities_impl(
         .iter()
         .map(|constant| (constant.name.clone(), constant.clone()))
         .collect::<HashMap<_, _>>();
+    let syntax_functions = program
+        .functions
+        .iter()
+        .map(|function| (function.name.clone(), function.clone()))
+        .collect::<HashMap<_, _>>();
+    let consts = resolve_const_int_decls(&consts, &syntax_functions).map_err(single_diagnostic)?;
     validate_const_array_lengths_in_program(&program, &consts).map_err(single_diagnostic)?;
     let (struct_names, enum_names, aliases) =
         collect_type_names(&program.structs, &program.enums, &program.type_aliases)
@@ -5868,6 +5874,177 @@ fn eval_const_int_expr(expr: &syntax::Expr) -> Option<i64> {
             }
         },
         _ => None,
+    }
+}
+
+fn resolve_const_int_decls(
+    consts: &HashMap<String, syntax::ConstDecl>,
+    functions: &HashMap<String, syntax::Function>,
+) -> Result<HashMap<String, syntax::ConstDecl>, Diagnostic> {
+    let mut resolved = consts.clone();
+    let mut values = HashMap::new();
+    for name in consts.keys() {
+        let mut resolving = HashSet::new();
+        match eval_const_int_decl(name, consts, functions, &mut values, &mut resolving)? {
+            Some(value) => {
+                if let Some(decl) = resolved.get_mut(name) {
+                    decl.expr = syntax::Expr::Literal(syntax::Literal::Int(value));
+                }
+            }
+            None => {
+                let decl = &consts[name];
+                if matches!(decl.ty, syntax::TypeName::Int)
+                    && const_int_expr_contains_call(&decl.expr)
+                {
+                    return Err(Diagnostic::new(
+                        "type",
+                        format!(
+                            "const {:?} requires a pure const fn integer expression",
+                            decl.name
+                        ),
+                    )
+                    .with_span(decl.line, decl.column));
+                }
+            }
+        }
+    }
+    Ok(resolved)
+}
+
+fn eval_const_int_decl(
+    name: &str,
+    consts: &HashMap<String, syntax::ConstDecl>,
+    functions: &HashMap<String, syntax::Function>,
+    values: &mut HashMap<String, i64>,
+    resolving: &mut HashSet<String>,
+) -> Result<Option<i64>, Diagnostic> {
+    if let Some(value) = values.get(name) {
+        return Ok(Some(*value));
+    }
+    let Some(decl) = consts.get(name) else {
+        return Ok(None);
+    };
+    if !resolving.insert(name.to_string()) {
+        return Err(Diagnostic::new(
+            "type",
+            format!("const {name:?} has a recursive initializer"),
+        )
+        .with_span(decl.line, decl.column));
+    }
+    let mut locals = HashMap::new();
+    let value = eval_const_int_expr_resolved(
+        &decl.expr,
+        consts,
+        functions,
+        values,
+        resolving,
+        &mut locals,
+    )?;
+    resolving.remove(name);
+    if let Some(value) = value {
+        values.insert(name.to_string(), value);
+    }
+    Ok(value)
+}
+
+fn eval_const_int_expr_resolved(
+    expr: &syntax::Expr,
+    consts: &HashMap<String, syntax::ConstDecl>,
+    functions: &HashMap<String, syntax::Function>,
+    values: &mut HashMap<String, i64>,
+    resolving: &mut HashSet<String>,
+    locals: &mut HashMap<String, i64>,
+) -> Result<Option<i64>, Diagnostic> {
+    match expr {
+        syntax::Expr::Literal(syntax::Literal::Int(value)) => Ok(Some(*value)),
+        syntax::Expr::VarRef { name, .. } => {
+            if let Some(value) = locals.get(name) {
+                return Ok(Some(*value));
+            }
+            eval_const_int_decl(name, consts, functions, values, resolving)
+        }
+        syntax::Expr::BinaryAdd { op, lhs, rhs, .. } => {
+            let lhs =
+                eval_const_int_expr_resolved(lhs, consts, functions, values, resolving, locals)?;
+            let rhs =
+                eval_const_int_expr_resolved(rhs, consts, functions, values, resolving, locals)?;
+            Ok(match (lhs, rhs) {
+                (Some(lhs), Some(rhs)) => Some(match op {
+                    syntax::ArithmeticOp::Add => lhs + rhs,
+                    syntax::ArithmeticOp::Sub => lhs - rhs,
+                    syntax::ArithmeticOp::Mul => lhs * rhs,
+                    syntax::ArithmeticOp::Div => lhs / rhs,
+                }),
+                _ => None,
+            })
+        }
+        syntax::Expr::Call { name, args, .. } => {
+            let Some(function) = functions.get(name) else {
+                return Ok(None);
+            };
+            if !function.is_const || function.is_extern || function.params.len() != args.len() {
+                return Ok(None);
+            }
+            let mut function_locals = HashMap::new();
+            for (param, arg) in function.params.iter().zip(args.iter()) {
+                let Some(value) = eval_const_int_expr_resolved(
+                    arg, consts, functions, values, resolving, locals,
+                )?
+                else {
+                    return Ok(None);
+                };
+                function_locals.insert(param.name.clone(), value);
+            }
+            eval_const_int_block(
+                &function.body,
+                consts,
+                functions,
+                values,
+                resolving,
+                &mut function_locals,
+            )
+        }
+        _ => Ok(None),
+    }
+}
+
+fn eval_const_int_block(
+    body: &[syntax::Stmt],
+    consts: &HashMap<String, syntax::ConstDecl>,
+    functions: &HashMap<String, syntax::Function>,
+    values: &mut HashMap<String, i64>,
+    resolving: &mut HashSet<String>,
+    locals: &mut HashMap<String, i64>,
+) -> Result<Option<i64>, Diagnostic> {
+    for stmt in body {
+        match stmt {
+            syntax::Stmt::Let { name, expr, .. } => {
+                let Some(value) = eval_const_int_expr_resolved(
+                    expr, consts, functions, values, resolving, locals,
+                )?
+                else {
+                    return Ok(None);
+                };
+                locals.insert(name.clone(), value);
+            }
+            syntax::Stmt::Return { expr, .. } => {
+                return eval_const_int_expr_resolved(
+                    expr, consts, functions, values, resolving, locals,
+                );
+            }
+            _ => return Ok(None),
+        }
+    }
+    Ok(None)
+}
+
+fn const_int_expr_contains_call(expr: &syntax::Expr) -> bool {
+    match expr {
+        syntax::Expr::Call { .. } => true,
+        syntax::Expr::BinaryAdd { lhs, rhs, .. } | syntax::Expr::BinaryCompare { lhs, rhs, .. } => {
+            const_int_expr_contains_call(lhs) || const_int_expr_contains_call(rhs)
+        }
+        _ => false,
     }
 }
 
